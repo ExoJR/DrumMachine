fcd4644beecad1494d762885ea2ea29e007b4f7f
Exo Jr <ravacemilian@yahoo.com>
2024-03-27 16:29:17 +0200

    I added display functionality

--- src/components/drumMachine.jsx
+++ src/components/drumMachine.jsx
@@ -18,47 +18,70 @@ const playSound = (btnId) => {
   }
 };
 
+// const powerBtn = () => {
+// const Pbtn = document.getElementById('power-btn');
+// if (Pbtn.classList.contains('off')) {
+//   console.log('Elementul are clasa "my-class".');
+// } else {
+//   console.log('Elementul nu are clasa "my-class".');
+// }
+// }
+
 function DrumMachine() {
+  const [soundsName, setSoundsName] = useState("");
+
+  
+
   useEffect(() => {
     const pressKey = (event) => {
-      const key = event.key
+      const key = event.key;
       const button = document.getElementById(key);
-       
+
       if (button) {
         button.classList.add("pressed");
 
         setTimeout(() => {
           button.classList.remove("pressed");
+          setSoundsName("");
         }, 100);
       }
 
       switch (event.key) {
         case "q":
           playSound("Q");
+          setSoundsName("clap");
           break;
         case "w":
           playSound("W");
+          setSoundsName("kick-drum");
           break;
         case "e":
           playSound("E");
+          setSoundsName("snare-drum");
           break;
         case "a":
           playSound("A");
+          setSoundsName("hi-hat-closed");
           break;
         case "s":
           playSound("S");
+          setSoundsName("tom-drum");
           break;
         case "d":
           playSound("D");
+          setSoundsName("crash-cymbal");
           break;
         case "z":
           playSound("Z");
+          setSoundsName("hi-hat-open");
           break;
         case "x":
           playSound("X");
+          setSoundsName("wood-block");
           break;
         case "c":
           playSound("C");
+          setSoundsName("ride-cymbal");
           break;
         default:
           break;
@@ -75,38 +98,128 @@ function DrumMachine() {
   return (
     <div id="drum-machine">
       <div id="drum-bank">
-        <div id="q" className="drum-pad" onClick={() => playSound("Q")}>
+        <div
+          id="q"
+          className="drum-pad"
+          onClick={() => {
+            setSoundsName("clap");
+            playSound("Q");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           Q<audio id="Q" className="clip" src={clap}></audio>
         </div>
-        <div id="w" className="drum-pad" onClick={() => playSound("W")}>
+        <div
+          id="w"
+          className="drum-pad"
+          onClick={() => {
+            playSound("W");
+            setSoundsName("kick-drum");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           W<audio id="W" className="clip" src={kickDrum}></audio>
         </div>
-        <div id="e" className="drum-pad" onClick={() => playSound("E")}>
+        <div
+          id="e"
+          className="drum-pad"
+          onClick={() => {
+            playSound("E");
+            setSoundsName("snare-drum");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           E<audio id="E" className="clip" src={snareDrum}></audio>
         </div>
-        <div id="a" className="drum-pad" onClick={() => playSound("A")}>
+        <div
+          id="a"
+          className="drum-pad"
+          onClick={() => {
+            playSound("A");
+            setSoundsName("hi-hat-closed");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           A<audio id="A" className="clip" src={hiHatClosed}></audio>
         </div>
-        <div id="s" className="drum-pad" onClick={() => playSound("S")}>
+        <div
+          id="s"
+          className="drum-pad"
+          onClick={() => {
+            playSound("S");
+            setSoundsName("tom-drum");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           S<audio id="S" className="clip" src={tomDrum}></audio>
         </div>
-        <div id="d" className="drum-pad" onClick={() => playSound("D")}>
+        <div
+          id="d"
+          className="drum-pad"
+          onClick={() => {
+            playSound("D");
+            setSoundsName("crash-cymbal");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           D<audio id="D" className="clip" src={crashCymbal}></audio>
         </div>
-        <div id="z" className="drum-pad" onClick={() => playSound("Z")}>
+        <div
+          id="z"
+          className="drum-pad"
+          onClick={() => {
+            playSound("Z");
+            setSoundsName("hi-hat-open");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           Z<audio id="Z" className="clip" src={hiHatOpen}></audio>
         </div>
-        <div id="x" className="drum-pad" onClick={() => playSound("X")}>
+        <div
+          id="x"
+          className="drum-pad"
+          onClick={() => {
+            playSound("X");
+            setSoundsName("wood-block");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           X<audio id="X" className="clip" src={woodBlock}></audio>
         </div>
-        <div id="c" className="drum-pad" onClick={() => playSound("C")}>
+        <div
+          id="c"
+          className="drum-pad"
+          onClick={() => {
+            playSound("C");
+            setSoundsName("ride-cymbal");
+            setTimeout(() => {
+              setSoundsName("");
+            }, 1000);
+          }}
+        >
           C<audio id="C" className="clip" src={rideCymbal}></audio>
         </div>
       </div>
       <div id="drum-control">
         <div className="logo">Drum Machine</div>
         <div id="display">
-          <p id="display-text"></p>
+          <p id="display-text">{soundsName}</p>
         </div>
         <div id="volume">
           <p>Volume</p>
@@ -115,7 +228,7 @@ function DrumMachine() {
         <div className="buttons">
           <div className="Power">
             <p>Power</p>
-            <button className="button-30" role="button"></button>
+            <button id="power-btn" className="button-30" role="button"></button>
           </div>
           <div className="Bank">
             <p>Bank</p>
